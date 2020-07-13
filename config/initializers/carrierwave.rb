d7de89f2e4978d6dbd04fe53a18d2f63ab8a03c9
sketch25 <sket.ch.life725@gmail.com>
2020-07-13 13:15:35 +0900

    修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'depbucket725'
-  config.asset_host = 'https://depbucket725.s3.amazonaws.com'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/depbucket725'
 end
